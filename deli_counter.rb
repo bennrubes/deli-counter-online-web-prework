4495d0e610464f9e1ee12ede91d9553a7d605a02
Sam Bennrubi <sbennrubi@gmail.com>
2018-11-06 00:05:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,30 @@
-def line(deli)
-  if deli == []
-  puts "The line is currently empty."
-  else
-    line_with_people = deli.each_with_index.map do |name, position|
-      "#{position + 1}. #{name}"
+require "pry"
+class Deli
+@@dispenser = 1
+
+  def line(deli)
+    if deli == []
+    puts "The line is currently empty."
+    else
+      line_with_people = deli.each_with_index.map do |name, position|
+        "#{position + 1}. #{name}"
+      end
+      puts "The line is currently: #{line_with_people.join(" ")}"
     end
-    puts "The line is currently: #{line_with_people.join(" ")}"
   end
-end
-
-def take_a_number(deli, name)
-  if deli == []
-    deli.push(name)
-    puts "Welcome, #{name}. You are number 1 in line."
-  else
-    deli.push(name)
-      puts "Welcome, #{name}. You are number #{deli.count} in line."
+  
+  def take_a_number(deli)
+      deli.push(@@dispenser)
+      puts "Welcome, your ticket number is #{@@dispenser} and you are #{deli.count} in line."
+      @@dispenser += 1
   end
-end
+  
+  def now_serving(deli)
+   deli == [] ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{deli.shift}.")
+  end
+end  
 
-def now_serving(deli)
- deli == [] ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{deli.shift}.")
-end
+new_deli = Deli.new
+new_deli.take_a_number([])
+new_deli.take_a_number([1])
+binding.pry
